88bbe215dc848a102f8136a6dbda4a34676842cb
Ilya S <sewd33p@gmail.com>
2022-12-12 00:17:48 +0700

    Update color scheme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,11 +2,11 @@ local wezterm = require "wezterm"
 
 return {
   font_size = 10,
-  font = wezterm.font "JetBrains Mono",
+  font = wezterm.font "JetBrains Mono NL",
 
   use_fancy_tab_bar = false,
-  hide_tab_bar_if_only_one_tab = true,
+  hide_tab_bar_if_only_one_tab = false,
 
-  color_scheme = "nightfox",
+  color_scheme = "Catppuccin Frappe",
   window_decorations = "RESIZE",
 }
